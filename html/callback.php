7fd1020dd635d7b2c4817753a08e4438d24c77c7
genzouw <genzouw@gmail.com>
2019-05-25 16:28:14 +0900

    Added logic when "code" is specified as response_type of authentication function of Amazon Cognito.

--- html/callback.php
+++ html/callback.php
@@ -2,16 +2,75 @@
   <body>
     <div id="container">
       <h1>Cognito Callback Page</h1>
+<?php
+if (getenv('COGNITO_RESPONSE_TYPE') === 'token') {
+    echo '
+      <h2>access_type=codeで認証手続きが行われました。</h2>
 
       <em>自動的に遷移します...</em>
-    </div>
-    <script>
-      // URLからフラグメント、つまり `#` より後ろの部分を取り出す
-      let fragment = document.location.href.replace(/^.*#/mgi, "");
-      // フラグメントから "id_token=~" の部分を取り出す
-      let idToken = fragment.split("&").find((text) => text.indexOf("id_token") === 0).replace(/^id_token=/mgi, "")
 
-      document.location = `/check_id_token.php?id_token=${idToken}`
-    </script>
+      <script>
+        // URLからフラグメント、つまり `#` より後ろの部分を取り出す
+        let fragment = document.location.href.replace(/^.*#/mgi, "");
+        // フラグメントから "id_token=~" の部分を取り出す
+        let idToken = fragment.split("&").find((text) => text.indexOf("id_token") === 0).replace(/^id_token=/mgi, "")
+
+        document.location = `/check_id_token.php?id_token=${idToken}`
+      </script>
+    ';
+} else {
+    echo '<h2>access_type=codeで認証手続きが行われました。</h2>';
+
+    // Cognitoに送るPOSTリクエストのボディ情報
+    $data = array(
+        'code' => $_GET['code'],
+        'client_id' => getenv('COGNITO_CLIENT_ID'),
+        'grant_type' => 'authorization_code',
+        'redirect_uri' => getenv('COGNITO_CALLBACK_URL'),
+        'client_secret' => getenv('COGNITO_CLIENT_SECRET'),
+    );
+
+    // Cognitoに送るPOSTリクエストのURL
+    $url = 'https://' . getenv('COGNITO_DOMAIN') . '.auth.' . getenv('COGNITO_REGION_ID') . '.amazoncognito.com/oauth2/token';
+
+    // CognitoにPOSTリクエストを送信し、レスポンスJSONテキスト情報を取得
+    $jwtJsonText = file_get_contents(
+        $url,
+        false,
+        stream_context_create(
+            array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => array(
+                        "Content-Type: application/x-www-form-urlencoded",
+                    ),
+                    'content' => http_build_query($data),
+                ),
+            )
+        )
+    );
+
+    if ($jwtJsonText) {
+        // ブラウザに"ID Token"を渡さずに、JavaScriptを使わずに認証が完了する
+        $jwt = (array) json_decode($jwtJsonText);
+
+        echo '<h3>ID Token</h3>';
+        echo '<pre>';
+        var_dump($jwt['id_token']);
+        echo '</pre>';
+
+        // ID Tokenをカンマ(".")で区切って2番目の要素にemailアドレス情報が付与されている
+        echo '<h3>ID Token(base64 decoded)</h3>';
+        echo '<pre>';
+        var_dump(
+            base64_decode(
+                explode('.', $jwt['id_token'])[1]
+            )
+        );
+        echo '</pre>';
+    }
+}
+?>
+    </div>
   </body>
 </html>
--- html/index.php
+++ html/index.php
@@ -6,9 +6,30 @@
       <h2>Start OAuth2</h2>
 
       <ul>
+<?php
+// 設定が必要な環境変数名の一覧
+$envVarNames = array(
+    'COGNITO_DOMAIN',
+    'COGNITO_REGION_ID',
+    'COGNITO_USERPOOL_ID',
+    'COGNITO_CLIENT_ID',
+    'COGNITO_CALLBACK_URL',
+    'COGNITO_RESPONSE_TYPE',
+);
+
+// 最低限必要な環境変数の設定が行われているかをチェック
+foreach ($envVarNames as $name) {
+    if (!getenv($name)) {
+        echo "<li><strong style=\"color:red\">\"${name}\" is not defined.</strong></li>";
+    }
+}
+?>
+      </ul>
+
+      <ul>
         <li>
           <strong>
-            <a href="https://<?php echo getenv('COGNITO_DOMAIN'); ?>.auth.<?php echo getenv('COGNITO_REGION_ID'); ?>.amazoncognito.com/login?response_type=token&client_id=<?php echo getenv('COGNITO_CLIENT_ID'); ?>&redirect_uri=http://localhost:8080/callback.php">LOGIN!</a>
+            <a href="https://<?php echo getenv('COGNITO_DOMAIN'); ?>.auth.<?php echo getenv('COGNITO_REGION_ID'); ?>.amazoncognito.com/login?response_type=<?php echo getenv('COGNITO_RESPONSE_TYPE'); ?>&client_id=<?php echo getenv('COGNITO_CLIENT_ID'); ?>&redirect_uri=<?php echo getenv('COGNITO_CALLBACK_URL'); ?>">LOGIN!</a>
           </strong>
         </li>
       </ul>
